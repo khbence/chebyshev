9ef98aeb44ac3a56d2dcdbd602488a18082c50ad
khbence <horvathb94@gmail.com>
2019-11-03 10:18:45 +0100

    calculates zeros at zeros points

--- include/chebyshev.h
+++ include/chebyshev.h
@@ -5,8 +5,8 @@ template<unsigned degree>
 class ChebyshevPolynom {
     constexpr static auto initParameters() {
         std::array<int, degree/2 + 1> param{};
-        decltype(param) old{1}; //1
-        param[0] = 1;
+        decltype(param) old{1}; //f(x) = 1
+        param[0] = 1; //f(x) = x
         if(!degree) { return param; }
         for(unsigned i = 0; i < degree-1; ++i) {
             auto tmp = param;
@@ -19,25 +19,34 @@ class ChebyshevPolynom {
         return param;
     }
 
+    constexpr static auto initPoints() {
+        std::array<double, degree> ps{};
+        for(unsigned j = 0; j < degree; ++j) {
+            ps[j] = std::cos(((2*static_cast<double>(j) - 1)*M_PI) / (2*static_cast<double>(degree)));
+        }
+        return ps;
+    }
+
     constexpr static auto parameters = initParameters();
+    constexpr static auto points = initPoints();
 
     template<std::size_t... N>
     constexpr static double polynom(double x, std::index_sequence<N...>) {
         return ((parameters[N] * std::pow(x, degree - (2*N))) + ...);
     }
 
-public:
     constexpr static double Px(double x) {
         return polynom(x, std::make_index_sequence<parameters.size()>());
     }
 
     //TODO multiply with fx, but first just return the polynom
 public:
-
-
     template<typename function>
     constexpr static auto getValuesAtPoints(function fx, double a, double b) {
-        //TODO get the points (zero points)
-        return 5;
+        std::array<double, degree> values{};
+        for(unsigned i = 0; i < values.size(); ++i) {
+            values[i] = Px(points[i]);
+        }
+        return values;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include "chebyshev.h"
 
 int main(int argc, char const *argv[]) {
-    //ChebyshevPolynom<2>::print();
-    std::cout << ChebyshevPolynom<4>::Px(2.0) << std::endl;
-    //auto chebyPoints = ChebyshevPolynom<4>::getValuesAtPoints([](double x) { return 1.0 / (1.0 + std::pow(x, 2)); }, -5.0, 5.0);
+    auto chebyPoints = ChebyshevPolynom<11>::getValuesAtPoints([](double x) { return 1.0 / (1.0 + std::pow(x, 2)); }, -5.0, 5.0);
+    for(auto e : chebyPoints) {
+        std::cout << e << std::endl;
+    }
     return EXIT_SUCCESS;
 }
